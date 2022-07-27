5e5d7aede463301379576d5cddfd7a1fad9bda84
Renan Venturi <renanventuri@pop-os.localdomain>
2022-07-27 19:36:39 -0300

    adicionando imagem/conteúdo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Image from './images/Scarecrow.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>olá mundo</h1>
-    </div>
-  );
+    <body>
+      <div className='all'>
+        <div>
+          <text>404 not found</text>
+        </div>
+        <div>
+        <div className='content'>
+          <img src={Image} alt="Imagem" />
+          </div>
+          <div className='texts'>
+            <h1>I have bad news <br/> for you</h1>
+            <h3>The page you are looking for might be removed or is temporarily unavailable</h3>
+            <button>Back to home page</button>
+          </div>
+          </div>
+        </div>
+    </body>
+  )
 }
 
 export default App;
+
