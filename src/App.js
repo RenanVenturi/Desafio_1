8878469fb329efd830ea45ded4d1703c7666dc05
Renan Venturi <renanventuri@pop-os.localdomain>
2022-07-29 07:42:32 -0300

    formatando código

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import Image from './images/Scarecrow.png'
 function App() {
   return (
     <body>
-      <div className='all'>
-        <div>
-          <text>404 not found</text>
-        </div>
-        <div>
-        <div className='content'>
-          <img src={Image} alt="Imagem" />
-          </div>
-          <div className='texts'>
-            <h1>I have bad news <br/> for you</h1>
-            <h3>The page you are looking for might be removed or is temporarily unavailable</h3>
-            <button>Back to home page</button>
-          </div>
+      <div className="all">
+        <h3>404 NOT FOUND</h3>
+        <div className="content"> 
+          <img src={Image} alt=""/>
+          <div>
+            <h1>I have bad news <br /> for you</h1>
+            <h2>The page you are looking for might be removed or is temporarily unavailable</h2>
+            <button>Back to home</button>
           </div>
         </div>
+      </div>
     </body>
   )
 }
